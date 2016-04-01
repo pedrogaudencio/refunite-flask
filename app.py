f827e4f6e5ee52d517c2cece56f49357df517001
Pedro Gaudencio <pmgaudencio@gmail.com>
2016-04-01 10:33:39 +0700

    users: dummy data for testing
    * admin@refunite.org : admin
    * test@refunite.org : test

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask.ext.security import (
     Security,
     SQLAlchemyUserDatastore,
+    utils,
 )
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -26,3 +27,27 @@ security = Security(app, user_datastore)
 # Blueprint registration
 from modules.users.views import user_login
 app.register_blueprint(user_login)
+
+# Create admin and test user
+@app.before_first_request
+def create_user():
+    db.create_all()
+    user_datastore.find_or_create_role(name='admin',
+                                       description='Administrator')
+    user_datastore.find_or_create_role(name='user',
+                                       description='User')
+
+    encrypted_admin_password = utils.encrypt_password('admin')
+    encrypted_user_password = utils.encrypt_password('test')
+    if not user_datastore.get_user('admin@refunite.org'):
+        user_datastore.create_user(email='admin@refunite.org',
+                                   password=encrypted_admin_password)
+    if not user_datastore.get_user('test@refunite.org'):
+        user_datastore.create_user(email='test@refunite.org',
+                                   password=encrypted_user_password)
+
+    db.session.commit()
+
+    user_datastore.add_role_to_user('admin@refunite.org', 'admin')
+    user_datastore.add_role_to_user('test@refunite.org', 'user')
+    db.session.commit()
